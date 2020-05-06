a32886c5c89cd9880fe0ab7d9c3b5819f1ce8ecd
jordanrob709 <jordanrob709@gmail.com>
2020-05-06 19:19:26 +0300

    added created general custom user

--- server/settings.py
+++ server/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user.apps.UserConfig',
+    'sme.apps.SmeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
