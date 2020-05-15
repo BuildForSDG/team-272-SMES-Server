b3ff0019795419a392647e530bc5f7651d3a0fd8
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-05-15 04:40:38 +0300

    feature-API-Update

--- sme/views.py
+++ sme/views.py
@@ -1,18 +1,44 @@
 from rest_framework import generics
+from rest_framework.generics import GenericAPIView
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+from django.contrib.auth import  authenticate
+
 
 from .models import SME
-from .serializers import SMESerializer
+from .serializers import SMECreateUserSerializer, SMESerializer
 
 # Create your views here.
+
+
+class SMELogin(APIView):
+    permission_classes = ()
+
+    def post(self, request,):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({"token": user.auth_token.key})
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
+class SMESignup(generics.CreateAPIView):
+    authentication_classes = ()
+    permission_classes = ()
+    serializer_class = SMECreateUserSerializer
 
 class SMEList(generics.ListCreateAPIView):
     queryset = SME.objects.all()
     serializer_class = SMESerializer
 
-class SMEDetail(generics.RetrieveAPIView):
+class SMEProfile(generics.RetrieveAPIView):
     queryset = SME.objects.all()
     serializer_class = SMESerializer
 
-class SMEUpdate(generics.RetrieveUpdateDestroyAPIView):
+class SMEProfileUpdate(generics.RetrieveUpdateDestroyAPIView):
     queryset = SME.objects.all()
     serializer_class = SMESerializer
+
