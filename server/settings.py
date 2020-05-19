94dab6527750c3958c34da8eed70b32232a81f2f
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-05-20 02:06:28 +0300

    static-files

--- server/settings.py
+++ server/settings.py
@@ -130,6 +130,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 AUTH_USER_MODEL = 'user.CustomUser'
 
 # media configurations
