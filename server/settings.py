d3b6edcedfee6a04715ab4420a339aedac92bbf9
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-05-20 03:15:25 +0300

    testing

--- server/settings.py
+++ server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wq9zs1n5hd$4mv66+d@3b#$$n$-vrp5)83$(e-z7h5u$o0fum0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
