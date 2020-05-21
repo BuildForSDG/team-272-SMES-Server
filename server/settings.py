2da0aea1a6252a76adb6fbf9274a87973de1806c
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-05-21 23:47:43 +0300

    password-reset

--- server/settings.py
+++ server/settings.py
@@ -157,3 +157,13 @@ REST_FRAMEWORK = {
 
 # Activate Django-Heroku
 # django_heroku.settings(locals())
+
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'u.business.link256@gmail.com'
+EMAIL_HOST_PASSWORD = 'Tryagain01'
