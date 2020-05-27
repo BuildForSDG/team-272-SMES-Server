625c0fd268f35733ce093762b150e6042fbed39f
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-05-27 22:55:15 +0300

    bug-fix-unused imports sme/views

--- sme/views.py
+++ sme/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
-from rest_framework.generics import GenericAPIView
+# from rest_framework.generics import GenericAPIView
 from rest_framework.views import APIView
-from rest_framework import status
+# from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
 from django.contrib.auth import  authenticate
