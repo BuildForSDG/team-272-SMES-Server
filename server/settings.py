9dfb62ba6567dada7f38760257d258fb8a58b382
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-06-07 14:38:49 +0300

    CORS configuration - resolving header errrors

--- server/settings.py
+++ server/settings.py
@@ -58,19 +58,56 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
 
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+
+    'corsheaders.middleware.CorsPostCsrfMiddleware',
+    
+
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = [
+    "https://127.0.0.1:4200",
+    "https://localhost:4200",
+    "http://127.0.0.1:4200",
+    "http://localhost:4200",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "https://127.0.0.1:4200",
+    "https://localhost:4200",
+    "http://127.0.0.1:4200",
+    "http://localhost:4200",
+]
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'server.urls'
 
